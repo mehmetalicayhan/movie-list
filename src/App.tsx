8179fd3e368a77db1c61d0fd47124dce9b83ce0d
mehmetalicayhan <mehmetalicyhn@gmail.com>
2024-05-15 02:02:03 +0300

    Project initialized

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="p-16">
+      <h1 className="text-4xl text-center">Hello World</h1>
+      <p className="text-center">This is a paragraph</p>
+    </div>
+  );
+}
+
+export default App;
