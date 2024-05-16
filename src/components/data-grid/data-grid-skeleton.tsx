9576c8314d8ff8ef140e49bce6d516dbd3e2b81b
mehmetalicayhan <mehmetalicyhn@gmail.com>
2024-05-16 04:07:15 +0300

    fix: movie list page skeleton is not visible

--- src/components/data-grid/data-grid-skeleton.tsx
+++ src/components/data-grid/data-grid-skeleton.tsx
@@ -4,7 +4,7 @@ const DataGridSkeleton = () => {
   return (
     <div className="flex w-full flex-col space-y-2">
       <Skeleton className="h-8 w-full rounded-lg" />
-      <Skeleton className="min-h-[800px] w-full rounded-lg" />
+      <Skeleton className="min-h-[600px] w-full rounded-lg" />
       <div className="flex items-center justify-between ">
         <Skeleton className="h-6 w-[250px]" />
         <Skeleton className="h-6 w-[200px]" />
--- src/pages/MovieList.tsx
+++ src/pages/MovieList.tsx
@@ -4,10 +4,10 @@ import ImagePlaceholder from "@/components/image-placeholder";
 import MovieFilter from "@/components/movie/movie-filter";
 import { INIITAL_SEARCH_TERM, QUERY_KEYS } from "@/constants";
 import { MovieService, SearchMovieResponse } from "@/services/MovieService";
+import { Column } from "@/types/DataGrid";
 import { Movie } from "@/types/Movie";
 import { useQuery } from "@tanstack/react-query";
 import { useEffect, useState } from "react";
-import { Column } from "@/types/DataGrid";
 import { useNavigate } from "react-router-dom";
 
 const MovieList = () => {
@@ -25,7 +25,7 @@ const MovieList = () => {
     setPage(1);
   }, [filters]);
 
-  const { data, isLoading, isRefetching } = useQuery<SearchMovieResponse>({
+  const { data, isLoading } = useQuery<SearchMovieResponse>({
     queryKey: [QUERY_KEYS.MOVIES, page, filters],
     queryFn: () => MovieService.getMovies({ ...filters, page }),
     enabled: filters.s.length > 2,
@@ -55,22 +55,21 @@ const MovieList = () => {
   return (
     <div className="mx-auto flex max-w-5xl flex-col items-center justify-center gap-2 px-2 py-8 sm:px-0 ">
       <MovieFilter filters={filters} setFilters={setFilters} />
-      {data && (
-        <DataGrid
-          data={data?.Search}
-          isEmpty={data?.Response === "False"}
-          isLoading={isLoading || isRefetching}
-          columns={columns}
-          rowKey="imdbID"
-          onRowClick={(row) => navigate(`/${row.imdbID}`)}
-        >
-          <DataGridPagination
-            page={page}
-            onPageChange={setPage}
-            totalResults={data?.totalResults}
-          />
-        </DataGrid>
-      )}
+
+      <DataGrid
+        data={data?.Search || []}
+        isEmpty={data?.Response === "False"}
+        isLoading={isLoading}
+        columns={columns}
+        rowKey="imdbID"
+        onRowClick={(row) => navigate(`/${row.imdbID}`)}
+      >
+        <DataGridPagination
+          page={page}
+          onPageChange={setPage}
+          totalResults={data?.totalResults || 0}
+        />
+      </DataGrid>
     </div>
   );
 };
